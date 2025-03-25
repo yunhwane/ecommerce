14b10978c81713811c3365963cf2fadbc594dfaa
yunhwan-dev <wjsdbsghks96@dgu.ac.kr>
2025-03-25 23:53:05 +0900

    first setting

---
+++ src/test/kotlin/com/yh/ecommerce/EcommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yh.ecommerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
